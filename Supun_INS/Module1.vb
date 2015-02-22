f83adf9bb1b93b57806cf0ecf4af66c9a51e10f7
Mihindu <mihinduwijesena@gmail.com>
2015-02-22 12:36:12 +0100

    Changed the database

--- Supun_INS/Module1.vb
+++ Supun_INS/Module1.vb
@@ -23,7 +23,7 @@
 
     Public d1 As New Drugs
     Public o1 As New Order
-    Public Const strVersion As String = "V1.2 Build 21022015_2344_Snapshot"
+    Public Const strVersion As String = "V3.0 Build 22022015_1230_Snapshot"
 
     Public dtColor As New classMDataGridL
     Public blnDrugPopUpFromMenu As Boolean = False
